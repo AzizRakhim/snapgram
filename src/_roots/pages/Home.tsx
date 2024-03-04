9baddec5b2e891cf14977112d755b7fa6e689a94
Aziz Rakhim <azizrakhim17@gmail.com>
2024-03-04 20:09:23 +0500

    feat: finished product card and moved to post edit

--- src/_roots/pages/Home.tsx
+++ src/_roots/pages/Home.tsx
@@ -14,13 +14,13 @@ const Home = () => {
     <div className="flex flex-1">
       <div className="home-container">
         <div className="home-posts">
-          <h2 className="h3-bold md:h2-bold text-left w-full ">Home Feed</h2>
+          <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
           {isPostLoading && !posts ? (
             <Loader />
           ) : (
             <ul className="flex flex-col flex-1 gap-9 w-full">
               {posts?.documents.map((post: Models.Document) => (
-                <PostCard post={post} />
+                <PostCard post={post} key={post.caption} />
               ))}
             </ul>
           )}
--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -1,14 +1,21 @@
+import { useUserContext } from "@/context/AuthContext";
+import { multiFormatDateString } from "@/lib/utils";
 import { Models } from "appwrite";
 import { Link } from "react-router-dom";
+import PostStats from "./PostStats";
 
 type PostCardProps = {
   post: Models.Document;
 };
 
 const PostCard = ({ post }: PostCardProps) => {
+  const { user } = useUserContext();
+
+  if (!post.creator) return;
+
   return (
     <div className="post-card">
-      <div className="flex between">
+      <div className="flex-between">
         <div className="flex items-center gap-3">
           <Link to={`/profile/${post.creator.$id}`}>
             <img
@@ -27,7 +34,7 @@ const PostCard = ({ post }: PostCardProps) => {
             </p>
             <div className="flex-center gap-2 text-light-3">
               <p className="subtle-semibold lg:small-regular">
-                {post.$createdAt}
+                {multiFormatDateString(post.$createdAt)}
               </p>
               -
               <p className="subtle-semibold lg:small-regular">
@@ -36,7 +43,40 @@ const PostCard = ({ post }: PostCardProps) => {
             </div>
           </div>
         </div>
+
+        <Link
+          to={`/update-post/${post.$id}`}
+          className={`${user.id !== post.creator.$id && "hidden"}`}
+        >
+          <img
+            src={"/assets/icons/edit.svg"}
+            alt="edit"
+            width={20}
+            height={20}
+          />
+        </Link>
       </div>
+
+      <Link to={`/posts/${post.$id}`}>
+        <div className="small-medium lg:base-medium py-5">
+          <p>{post.caption}</p>
+          <ul className="flex gap-1 mt-2">
+            {post.tags.map((tag: string) => (
+              <li key={tag} className="text-light-3">
+                #{tag}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <img
+          src={post.imageUrl || "/assets/icons/profile-placeholder.svg"}
+          className="post-card_img"
+          alt="post image"
+        />
+      </Link>
+
+      <PostStats post={post} userId={user.id} />
     </div>
   );
 };
